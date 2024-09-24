5ee6ee6ad5ad117fb8c918d259d14f93ab9ac21b
KufzuK <vologin.vv@phystech.edu>
2024-09-24 10:53:45 +0300

    clk6 now clk6 not 12 and it works using 1 reg instead of 2

--- freq_del/freq_del.v
+++ freq_del/freq_del.v
@@ -6,13 +6,11 @@ module freq_del
     
     reg [2: 0]  counter;
     reg [2: 0] counter2;
-    reg            clk6;
     wire           imp5;
+    wire           clk6;
 
+    assign clk6 = (counter   > 3'h2);
     assign imp5 = (counter2 == 3'h4);
-
-    always @(posedge clk)
-        clk6     <= ~reset ? ((counter  == 3'h5) ? ~clk6 :            clk6) :        ~reset;
    
     always @(posedge clk)
         counter  <= ~reset ? ((counter  == 3'h5) ?  3'h0 : counter  + 3'h1) : {2'h0, ~reset};
