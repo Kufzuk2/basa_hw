f588ae72e907c56b27c2e4c4e5842b709d716cf3
kufzuk <vologin.vadim.v@gmail.com>
2024-10-10 17:42:54 +0300

    pr_coder parametrized

--- pr_coder/coder_test.v
+++ pr_coder/coder_test.v
@@ -1,17 +1,28 @@
 `timescale 1ns/100ps
 `define TESTNUM 50
 
-module dec_test;
+module dec_test
+#(
+    parameter NUM_SIZE = 16,
+    parameter OUT_SIZE = $clog2(NUM_SIZE)
+);
 
-    reg  [7: 0] num;
-    reg         clk;
-    wire [2: 0] out;
+    reg  [NUM_SIZE - 1: 0] num;
+    wire [OUT_SIZE - 1: 0] out;
+    reg                    clk;
+    
+
+
+    localparam MAX_OFFSET = $clog2(NUM_SIZE - OUT_SIZE);
+    reg  [MAX_OFFSET - 1: 0] offset;
 
     always 
         #1 clk = ~clk;
    
 
-    pr_coder pr_coder
+    pr_coder 
+        #(.NUM_SIZE(NUM_SIZE), .OUT_SIZE(OUT_SIZE))
+    pr_coder
     (
          .num (num),
          .out (out)
@@ -27,6 +38,24 @@ module dec_test;
             #10;
 
             $display("N = %d = %b -->>  %d", num, num, out);
+        end
+        
+            $display("************************** \n",);
+///   here with offset because almost each random number has 1 as 1st digit in
+///    binary
+       
+
+        for (integer i = 0; i < `TESTNUM; i = i + 1) begin
+            #10;
+            $display("  ");
+
+            offset = $random;
+            num    = $random >> offset;
+            num    =     num >> offset;
+
+            #10;
+
+            $display("N = %d = %b -->>  %d,  (offset = %d)", num, num, out, offset);
         end
         $finish;
 	end
--- pr_coder/pr_coder.v
+++ pr_coder/pr_coder.v
@@ -1,33 +1,24 @@
-`define MASK_1    8'd1  
-`define MASK_2    8'd2  
-`define MASK_4    8'd4  
-`define MASK_8    8'd8  
-`define MASK_16   8'd16  
-`define MASK_32   8'd32  
-`define MASK_64   8'd64 
-`define MASK_128  8'd128  
-`define MASK_256  8'd256  
-`define MASK_BEDA 3'd0  
-
-
-////
-// question: что тут лучше делать с нулем на входе? сделать нумерацию от 1 до 8?
-////
-
-
 module pr_coder
+#(
+    parameter NUM_SIZE = 8,
+    parameter OUT_SIZE = $clog2(NUM_SIZE)
+)
 (
-    input  [7: 0] num,
-    output [2: 0] out
+    input  [NUM_SIZE - 1: 0] num,
+    output [OUT_SIZE - 1: 0] out
 );
+    wire [OUT_SIZE - 1: 0] tmp [NUM_SIZE: 0];
 
-    assign out = (num & `MASK_128) ? 3'h7:
-                 (num & `MASK_64 ) ? 3'h6:
-                 (num & `MASK_32 ) ? 3'h5:
-                 (num & `MASK_16 ) ? 3'h4:
-                 (num & `MASK_8  ) ? 3'h3:
-                 (num & `MASK_4  ) ? 3'h2:
-                 (num & `MASK_2  ) ? 3'h1:
-                 (num & `MASK_1  ) ? 3'h0:
-                               `MASK_BEDA;
+
+    generate
+        genvar i;
+        for (i = NUM_SIZE; i >= 0; i = i - 1) begin
+            if (i == NUM_SIZE)
+                assign tmp[i] = 'h0;
+            else
+                assign tmp[i] = ((tmp[i + 1] == 'h0) & num[i]) ? i : tmp[i + 1];
+        end
+    endgenerate
+
+    assign out = tmp[0];
 endmodule
