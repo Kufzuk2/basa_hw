a42ad8315046a96d828b384169f629aa7f20e9ef
kufzuk <vologin.vadim.v@gmail.com>
2024-10-10 13:34:35 +0300

    log parametrized

--- log/log2.v
+++ log/log2.v
@@ -1,30 +1,28 @@
-`define MASK_1    8'd1  
-`define MASK_2    8'd2  
-`define MASK_4    8'd4  
-`define MASK_8    8'd8  
-`define MASK_16   8'd16  
-`define MASK_32   8'd32  
-`define MASK_64   8'd64 
-`define MASK_128  8'd128  
-`define MASK_256  8'd256  
-`define MASK_BEDA 3'd0  
+`define MASK_1    8'h1 
 
 module log2
+#(
+    parameter SIZE_IN  = 8,
+    parameter SIZE_OUT = $clog2(SIZE_IN)
+)
+
 (
-    input  wire [7: 0] num,
-    output wire [2: 0] degree
+    input  wire [SIZE_IN  - 1: 0]    num,
+    output wire [SIZE_OUT - 1: 0] degree
 );
+    wire [SIZE_OUT - 1: 0] tmp [SIZE_IN: 0];
+   
 
-    assign degree = { 3{(`MASK_1 == (num >> 0))}} & 3'h0 |
-                    { 3{(`MASK_1 == (num >> 1))}} & 3'h1 |   
-                    { 3{(`MASK_1 == (num >> 2))}} & 3'h2 |  
-                    { 3{(`MASK_1 == (num >> 3))}} & 3'h3 |  
-                    { 3{(`MASK_1 == (num >> 4))}} & 3'h4 |  
-                    { 3{(`MASK_1 == (num >> 5))}} & 3'h5 |  
-                    { 3{(`MASK_1 == (num >> 6))}} & 3'h6 |  
-                    { 3{(`MASK_1 == (num >> 7))}} & 3'h7 ;  
-                    // i suppose that there are no other options
+    generate 
+        genvar i;
+        for (i = 0; i < SIZE_IN + 1; i = i + 1) begin
+            if (i == 0)
+                assign tmp[i] = 'h0;
+            else
+                assign tmp[i] = tmp[i - 1] | ({SIZE_OUT{(`MASK_1 == (num >> i))}} & i);
+        end
+    endgenerate
 
+    assign degree = tmp[SIZE_IN];
 endmodule
     
-
--- log/log_test.v
+++ log/log_test.v
@@ -1,63 +1,38 @@
 `timescale 1ns/100ps
 `define TESTNUM 50
 
-module even_test;
+module even_test
+#(
+    parameter SIZE_IN  = 16,
+    parameter SIZE_OUT = $clog2(SIZE_IN)
+);
 
-    wire [2: 0] out;
-    reg         clk;
-    reg  [7: 0] num;
+    wire [SIZE_OUT - 1: 0] out;
+    reg  [SIZE_IN  - 1: 0] num;
+    reg                    clk;
 
+    integer val = 1;
 
     always 
         #1 clk = ~clk;
    
 
-    log2 log2
+    log2 
+    #(.SIZE_IN(SIZE_IN), .SIZE_OUT(SIZE_OUT))
+    log2
     (
         .num    (num),
         .degree (out)
     );
 
     initial begin 
-        #10;
-        num = 1;
-        #10;
-        $display("num = %d  degree = %d ", num, out);
-
-        #10;
-        num = 2;
-        #10;
-        $display("num = %d  degree = %d ", num, out);
-
-        #10;
-        num = 4;
-        #10;
-        $display("num = %d  degree = %d ", num, out);
-
-        #10;
-        num = 8;
-        #10;
-        $display("num = %d  degree = %d ", num, out);
-
-        #10;
-        num = 16;
-        #10;
-        $display("num = %d  degree = %d ", num, out);
-
-        #10;
-        num = 32;
-        #10;
-        $display("num = %d  degree = %d ", num, out);
-
-        #10;
-        num = 64;
-        #10;
-        $display("num = %d  degree = %d ", num, out);
-
-        #10;
-        num = 128;
-        #10;
-        $display("num = %d  degree = %d ", num, out);
+        for (integer i = 0; i < SIZE_IN; i = i + 1) begin
+            #10;
+            num = val;
+            val = val * 2;
+            #10;
+            $display("num = %d  degree = %d ", num, out);
+        end
 
         $finish;
 	end
